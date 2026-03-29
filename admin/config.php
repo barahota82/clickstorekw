27a929708392950efc02c72fb04b6fc374371ebc
barahota82 <barahota.82@gmail.com>
2026-03-30 02:32:30 +0300

    Update config.php

--- admin/config.php
+++ admin/config.php
@@ -1,5 +1,5 @@
 <?php
 return [
     'admin_username' => 'admin',
-    'admin_password_hash' => '$2y$10$wWj8m6L8z6J1k2m7uQm3uOYq2L8QxJj5v5NwB8rRkR4hDk5mZQ9hS'
+    'admin_password_hash' => '$2y$10$FzsDl6zuEWW4aj79ch4JmulP/3OTVnqC93UBjffXZHVyzpZAxSJVm'
 ];
