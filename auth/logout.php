565b1fa0a8562345c1ac0f547f71ccf5987d426b
barahota82 <barahota.82@gmail.com>
2026-04-01 22:41:16 +0300

    Update logout.php

--- auth/logout.php
+++ auth/logout.php
@@ -1,16 +1,12 @@
 <?php
 declare(strict_types=1);
 
-session_start();
-
 require_once __DIR__ . '/../config.php';
 
-/* ===== CLEAR SESSION ===== */
 unset($_SESSION['customer_auth']);
 unset($_SESSION['pending_customer_email']);
 unset($_SESSION['pending_customer_id']);
 
-/* ===== RESPONSE ===== */
 json_response(true, [
     'message' => 'Logged out successfully.'
 ]);
