b3f885862e8a4a6004d6bf4d91e3d67f6a4b47f2
barahota82 <barahota.82@gmail.com>
2026-04-01 04:22:28 +0300

    Update logout.php

--- auth/logout.php
+++ auth/logout.php
@@ -1,1 +1,13 @@
+<?php
+declare(strict_types=1);
 
+session_start();
+require_once __DIR__ . '/db.php';
+
+unset($_SESSION['customer_auth']);
+unset($_SESSION['pending_customer_email']);
+
+json_response([
+    'ok' => true,
+    'message' => 'Logged out successfully.'
+]);
