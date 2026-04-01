07391e51350d6a4c69fd4280ad919eab30ffe9a9
barahota82 <barahota.82@gmail.com>
2026-04-01 04:21:52 +0300

    Update status.php

--- auth/status.php
+++ auth/status.php
@@ -1,1 +1,18 @@
+<?php
+declare(strict_types=1);
 
+session_start();
+require_once __DIR__ . '/db.php';
+
+if (!isset($_SESSION['customer_auth']) || !is_array($_SESSION['customer_auth'])) {
+    json_response([
+        'ok' => true,
+        'logged_in' => false
+    ]);
+}
+
+json_response([
+    'ok' => true,
+    'logged_in' => true,
+    'customer' => $_SESSION['customer_auth']
+]);
