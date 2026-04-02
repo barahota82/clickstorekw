aaa91d4b55fa1f234a49784e0b7b808c42fca106
barahota82 <barahota.82@gmail.com>
2026-04-02 06:44:13 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,11 @@
 <?php
 declare(strict_types=1);
 
+session_start();
+
+require_once __DIR__ . '/../config.php';
+declare(strict_types=1);
+
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
