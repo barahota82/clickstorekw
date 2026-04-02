7ac5c746cf49963488982eea445d468c059a47dc
barahota82 <barahota.82@gmail.com>
2026-04-02 18:15:13 +0300

    Update logout.php

--- auth/logout.php
+++ auth/logout.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 require_once __DIR__ . '/../config.php';
 
 unset($_SESSION['customer_auth']);
+unset($_SESSION['pending_customer_auth']);
 unset($_SESSION['pending_customer_email']);
 unset($_SESSION['pending_customer_id']);
 
