96d316e2d2b9d5413bb91caec600187f314ac96d
barahota82 <barahota.82@gmail.com>
2026-04-04 01:22:18 +0300

    Update .env.php

--- .env.php
+++ .env.php
@@ -1,8 +1,7 @@
 <?php
 return [
-    'SMTP_USER' => 'info@clickstorekw.com',
-    'SMTP_PASS' => 'Admin@Hem@3282',
-
-    'DB_USER' => 'clickst1_click_user',
-    'DB_PASS' => 'Admin@Hem@3282',
+    'SMTP_USER' => '',
+    'SMTP_PASS' => '',
+    'DB_USER' => '',
+    'DB_PASS' => '',
 ];
